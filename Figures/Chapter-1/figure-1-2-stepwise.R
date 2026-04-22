2e77cf8c85cb30ce011ec32535d4491dfe2a4f6f
Daniel Garcia Rasines <daniel.garciarasines@cunef.edu>
2026-04-22 16:08:13 +0200

    Add GitHub Action to generate Figure 1.1

---
+++ Figures/Chapter-1/figure-1-2-stepwise.R
@@ -0,0 +1,41 @@
+library(MASS)
+source(file.path("Figures", "theme.R"))
+
+B <- 1e4
+n <- 80
+p <- 20
+
+p_vals <- c()
+
+set.seed(111)
+for (i in seq_len(B)) {
+  X <- data.frame(matrix(rnorm(n * p), ncol = p))
+  y <- rnorm(n)
+
+  min_model <- lm(y ~ 1, data = X)
+  max_model <- formula(lm(y ~ ., data = X))
+
+  fit_step <- stepAIC(
+    min_model,
+    direction = "forward",
+    scope = max_model,
+    trace = FALSE
+  )
+
+  p_vals <- c(p_vals, summary(fit_step)$coefficients[, 4])
+}
+
+df <- data.frame(p_value = p_vals)
+
+p_plot <- ggplot(df, aes(x = p_value)) +
+  stat_ecdf(geom = "step", linewidth = 0.4, color = "black") +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "black") +
+  coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
+  labs(
+    x = "p-value",
+    y = "Empirical CDF",
+    title = "Figure 1.2: Stepwise p-values"
+  ) +
+  theme_book
+
+ggsave(file.path("Figures", "Chapter-1", "stepwise.pdf"), plot = p_plot, width = 5, height = 5)
--- R/CSI.R
+++
@@ -1,8 +0,0 @@
-# R/CSI.R
-
-# Shared theme and libraries
-
-library(ggplot2)
-library(dplyr)
-
-# Additional libraries can be added here
