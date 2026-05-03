2cd1167df3bcf1c73eedd7d044b6d7100155ac98
Daniel Garcia Rasines <daniel.garciarasines@cunef.edu>
2026-05-03 18:08:08 +0200

    Add Figure 6-7 R script for histogram plotting

---
+++ Figures/Chapter-6/Figure-6-7.R
@@ -0,0 +1,36 @@
+source(file.path("Figures", "theme.R"))
+
+n <- 5000
+n_selected <- 100
+
+set.seed(123)
+
+theta <- rexp(n, rate = 1)
+y <- rnorm(n, mean = theta, sd = 1)
+
+selected <- rank(-y, ties.method = "first") <= n_selected
+
+df <- data.frame(
+  y = y,
+  selected = selected
+)
+
+p_plot <- ggplot(df, aes(x = y)) +
+  geom_histogram(
+    data = subset(df, !selected),
+    bins = 70,
+    fill = "grey80",
+    color = "black",
+    linewidth = 0.2
+  ) +
+  geom_histogram(
+    data = subset(df, selected),
+    bins = 70,
+    fill = "grey35",
+    color = "black",
+    linewidth = 0.2
+  ) +
+  labs(x = "z values", y = "Frequency") +
+  theme_book
+
+ggsave(file.path("Figures", "Chapter-6", "fig-6-7.pdf"), plot = p_plot, width = 5, height = 5)
