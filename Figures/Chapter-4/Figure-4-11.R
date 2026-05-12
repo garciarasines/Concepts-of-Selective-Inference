a5db61bd49b53efda0246ecb0229dba70976d69f
Daniel Garcia Rasines <daniel.garciarasines@cunef.edu>
2026-05-12 15:14:38 +0200

    Update Figure-4-11.R

--- Figures/Chapter-4/Figure-4-11.R
+++ Figures/Chapter-4/Figure-4-11.R
@@ -67,4 +67,4 @@ p_plot <- ggplot(
     axis.text = element_text(size = 10)
   )
 
-ggsave(file.path("Figures", "Outputs", "fig-4-11.pdf"), plot = p_plot, width = 4, height = 3)
+ggsave(file.path("Figures", "Outputs", "fig-4-11.pdf"), plot = p_plot, width = 6, height = 4)
