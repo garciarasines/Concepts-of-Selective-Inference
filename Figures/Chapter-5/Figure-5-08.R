084e42fffa9d0b94749864b39639224dcbb30939
Daniel Garcia Rasines <daniel.garciarasines@cunef.edu>
2026-05-26 15:37:05 +0200

    Change output file name to fig-5-08.pdf

--- Figures/Chapter-5/Figure-5-08.R
+++ Figures/Chapter-5/Figure-5-08.R
@@ -176,4 +176,4 @@ p_plot <- ggplot(df_long, aes(x = t, y = value, color = method, linetype = metho
   theme_book +
   theme(legend.position = "bottom")
 
-ggsave(file.path("Figures", "Outputs", "fig-5-09.pdf"), plot = p_plot, width = 4, height = 3)
+ggsave(file.path("Figures", "Outputs", "fig-5-08.pdf"), plot = p_plot, width = 4, height = 3)
