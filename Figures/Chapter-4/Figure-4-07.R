5a308b22c3c2a98280b3b237beb2ec789a12f8f5
Daniel Garcia Rasines <daniel.garciarasines@cunef.edu>
2026-07-25 18:25:16 +0200

    Refactor CI calculations and plotting in Figure-4-07

--- Figures/Chapter-4/Figure-4-07.R
+++ Figures/Chapter-4/Figure-4-07.R
@@ -10,93 +10,95 @@ p <- dim(X)[2]
 
 X <- scale(X, TRUE, TRUE)
 
+lambda <- 12
+
 gfit <- glmnet(X, y, standardize = FALSE)
-beta <- coef(gfit, x = X, y = y, s = 7, exact = TRUE)[-1]
+beta <- coef(gfit, x = X, y = y, s = lambda, exact = TRUE)[-1]
 
-lambda <- 7
+# Selective CIs
+X_int <- cbind(rep(1, n), X)
+beta_hat <- solve(t(X_int)%*%X_int)%*%t(X_int)%*%y
+sigma_hat <- sqrt(sum((y - X_int%*%beta_hat)^2)/(n - p - 1))
 
-beta_hat <- solve(t(X)%*%X)%*%t(X)%*%y
-sigma_hat <- sqrt(sum((y - X%*%beta_hat)^2)/(n - p))
-
-out <- fixedLassoInf(X, y, beta, lambda, sigma = sigma_hat)
+out <- fixedLassoInf(X, y, beta, n*lambda, sigma = sigma_hat)
 lower_ad <- out$ci[, 1]
 upper_ad <- out$ci[, 2]
 
+# Non-selective CIs
 d <- dim(out$ci)[1]
 s <- which(beta != 0)
-Xs <- X[, s, drop = FALSE]
-
+Xs <- cbind(rep(1, n), X[, s, drop = FALSE])
 beta_hat <- solve(t(Xs)%*%Xs)%*%t(Xs)%*%y
-yhat <- Xs%*%beta_hat
-sqrt(sum((y - yhat)^2)/(n - p))
 
-sigma_hat <- sqrt(sum((y - Xs%*%beta_hat)^2)/(n - d))
-upper_un <- beta_hat + sqrt(diag(solve(t(Xs)%*%Xs)))*sigma_hat*qnorm(0.95)
-lower_un <- beta_hat - sqrt(diag(solve(t(Xs)%*%Xs)))*sigma_hat*qnorm(0.95)
+upper_un <- (beta_hat + sqrt(diag(solve(t(Xs)%*%Xs)))*sigma_hat*qnorm(0.95))[-1]
+lower_un <- (beta_hat - sqrt(diag(solve(t(Xs)%*%Xs)))*sigma_hat*qnorm(0.95))[-1]
 
-var_names <- toupper(colnames(diabetes$x)[s])
+var_names <- colnames(diabetes$x)[s]
 
-df_un <- data.frame(
-  x = 1:d - 0.12,
-  lower = as.numeric(lower_un),
-  upper = as.numeric(upper_un)
+df_plot <- data.frame(
+  variable = factor(rep(var_names, 2), levels = var_names),
+  index = rep(seq_len(d), 2),
+  method = factor(
+    rep(c("Unadjusted", "Selective"), each = d),
+    levels = c("Unadjusted", "Selective")
+  ),
+  estimate = rep(as.numeric(beta_hat[-1]), 2),
+  lower = c(as.numeric(lower_un), as.numeric(lower_ad)),
+  upper = c(as.numeric(upper_un), as.numeric(upper_ad))
 )
 
-df_ad <- data.frame(
-  x = 1:d + 0.12,
-  lower = as.numeric(lower_ad),
-  upper = as.numeric(upper_ad)
-)
+df_plot$x <- df_plot$index + ifelse(df_plot$method == "Unadjusted", -0.12, 0.12)
 
-cap_width <- 0.07
-
-p_plot <- ggplot() +
-  geom_segment(
-    data = df_un,
-    aes(x = x, xend = x, y = lower, yend = upper),
-    linewidth = 1,
+p_plot <- ggplot(df_plot, aes(x = x)) +
+  geom_hline(yintercept = 0, linewidth = 0.3) +
+  geom_errorbar(
+    data = subset(df_plot, method == "Unadjusted"),
+    aes(ymin = lower, ymax = upper),
+    width = 0.06,
+    linewidth = 0.5,
+    linetype = "11",
+    color = "grey45"
+  ) +
+  geom_point(
+    data = subset(df_plot, method == "Unadjusted"),
+    aes(y = estimate, shape = method),
+    size = 1.8,
+    color = "grey45"
+  ) +
+  geom_errorbar(
+    data = subset(df_plot, method == "Selective"),
+    aes(ymin = lower, ymax = upper),
+    width = 0.12,
+    linewidth = 0.9,
     color = "black"
   ) +
-  geom_segment(
-    data = df_un,
-    aes(x = x - cap_width, xend = x + cap_width, y = lower, yend = lower),
-    linewidth = 1,
+  geom_point(
+    data = subset(df_plot, method == "Selective"),
+    aes(y = estimate, shape = method),
+    size = 2.2,
     color = "black"
   ) +
-  geom_segment(
-    data = df_un,
-    aes(x = x - cap_width, xend = x + cap_width, y = upper, yend = upper),
-    linewidth = 1,
-    color = "black"
-  ) +
-  geom_segment(
-    data = df_ad,
-    aes(x = x, xend = x, y = lower, yend = upper),
-    linewidth = 1,
-    color = "grey50"
-  ) +
-  geom_segment(
-    data = df_ad,
-    aes(x = x - cap_width, xend = x + cap_width, y = lower, yend = lower),
-    linewidth = 1,
-    color = "grey50"
-  ) +
-  geom_segment(
-    data = df_ad,
-    aes(x = x - cap_width, xend = x + cap_width, y = upper, yend = upper),
-    linewidth = 1,
-    color = "grey50"
-  ) +
-  geom_hline(yintercept = 0, linewidth = 0.4, color = "grey70") +
   scale_x_continuous(
-    breaks = 1:d,
+    breaks = seq_len(d),
     labels = var_names
   ) +
-  coord_cartesian(
-    xlim = c(0.5, d + 0.5),
-    ylim = c(min(c(lower_un, lower_ad)) - 20, max(c(upper_un, upper_ad)) + 20)
+  scale_shape_manual(values = c(
+    "Unadjusted" = 1,
+    "Selective" = 16
+  )) +
+  labs(
+    x = NULL,
+    y = "Coefficient",
+    shape = NULL
   ) +
-  labs(x = NULL, y = "CIs") +
-  theme_book
+  theme_book +
+  theme(
+    legend.position = c(0.68, 0.92),
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank()
+  )
 
 ggsave(file.path("Figures", "Outputs", "fig-4-07.pdf"), plot = p_plot, width = 4, height = 3)
+
